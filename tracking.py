ce87102bfa308305470d8d82db3cc299e3c4bb0c
uncleurdnot <qminora@gmail.com>
2016-10-12 17:26:03 -0700

    fixed conflicts

--- tracking.py
+++ tracking.py
@@ -1,37 +1,27 @@
+#Quentin Minor, Babak Chehraz, 
 import numpy
 import cv2
 import datetime
 import time
-<<<<<<< HEAD
-import argparse
-=======
->>>>>>> 6af1b033ed3aeb4420bbaf873dd90c8c905cf552
 
-	
-bg = cv2.createBackgroundSubtractorMOG2()
-sx = 21 #Must be postive and odd
-sy = 21 #Must be positive & odd
-firstFrame = None
-i = 0
-res = 60
-
-def getVideo(tf, path):
+def getVideo(tf, path):#Get video from path or from webcam
 	if tf == False:
 		return cv2.VideoCapture(0)
 	else:
 		return cv2.VideoCapture(path)
 
-def init(G):
+def init(G):#initialzie first frame
 	return G
 
-def play(tf, path):
+def play(tf, path, pause):
+	#varriable declaration
 	bg = cv2.createBackgroundSubtractorMOG2()
 	sx = 21 #Must be postive and odd
 	sy = 21 #Must be positive & odd
 	firstFrame = None
 	i = 0
 	res = 60
-
+	#Video Object
 	cap = getVideo(tf, path)
 	while(True):
 		# Capture frame-by-frame
@@ -46,12 +36,10 @@ def play(tf, path):
 		# create threshold
 		diff = cv2.absdiff(firstFrame, gray)
 		retval, threshold = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
-		#Timestamp, copied from 
-		cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-			(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-		#cnts = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
 		thresh = cv2.dilate(threshold, None, iterations=2)
+		#Find contours
 		cnts = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+		#Draw rectangle around object
 		x,y,w,h = cv2.boundingRect(cnts)
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 		
@@ -59,36 +47,11 @@ def play(tf, path):
 		#(Name, Source)
 		cv2.imshow('Video Feed', frame)
 		cv2.imshow('Threshold', threshold)
+		#Reinitialize first frame
 		firstFrame = init(gray)
-<<<<<<< HEAD
-		continue
-	# create threshold
-	diff = cv2.absdiff(firstFrame, gray)
-	retval, threshold = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
-	#Timestamp, this was copy-pasted, NEEDS TO BE EDITED TO AVOID PLAGARISM
-	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-	thresh = cv2.dilate(threshold, None, iterations=2)
-	cnts = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-	x,y,w,h = cv2.boundingRect(cnts)
-	cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-	
-	# Display the resulting frame
-	#(Name, Source)
-	cv2.imshow('Video Feed', frame)
-	cv2.imshow('Threshold', threshold)
-	firstFrame = init(gray)
-	#Exit with the Q key, will figure out how to change to esc
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-# When everything done, release the capture, dont touch this
-cap.release()
-cv2.destroyAllWindows()
-=======
 		#Exit with the Q key, will figure out how to change to esc
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
-	# When everything done, release the capture, dont touch this
+	# Cleanup
 	cap.release()
 	cv2.destroyAllWindows()
->>>>>>> 6af1b033ed3aeb4420bbaf873dd90c8c905cf552
